cc1c8a9dc5860417afb41162d80edb8625467e5b
noweaj31 <noweajeel31@gmail.com>
2023-06-21 01:22:02 +0900

    feat:mainsection

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".main-slick").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    arrows: true,
+    prevArrow: $("#aro1_prev"),
+    nextArrow: $("#aro1_next"),
+  });
+});
