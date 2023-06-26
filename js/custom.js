ef786c120d46b609dba528dfa3e1613390429aa7
noweaj31 <noweajeel31@gmail.com>
2023-06-27 00:37:40 +0900

    feat:mainpage

--- js/custom.js
+++ js/custom.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
     autoplaySpeed: 2000,
     
   });
+  
   $(".img-slick").slick({
     slidesToShow: 13.7,
     slidesToScroll: 4,
@@ -16,6 +17,7 @@ $(document).ready(function () {
     prevArrow: $("#aro_prev"),
     nextArrow: $("#aro_next"),
   });
+  
   $(".item-container").slick({
     slidesToShow: 4,
     slidesToScroll: 4,
@@ -24,6 +26,18 @@ $(document).ready(function () {
     arrows: true,
     prevArrow: $("#aro__prev"),
     nextArrow: $("#aro__next"),
+  });
+  
+  $(".interior-img").slick({
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    arrows: true,
+    infinite: false,
+    arrows: true,
+    prevArrow: $("#aro___prev"),
+    nextArrow: $("#aro___next"),
   });
   
 });
