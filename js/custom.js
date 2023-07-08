2c409f0d40de6e794315d69fabcac761d70608e5
noweaj31 <noweajeel31@gmail.com>
2023-07-08 13:52:27 +0900

    feat:반응형 수정

--- js/custom.js
+++ js/custom.js
@@ -1,136 +1,126 @@
-$(document).ready(function () {
-  $(".slick-box").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    draggable:false,
-  });
+// $(document).ready(function () {
+//   $(".slick-box").slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//     draggable:false,
+//   });
   
 
-  $(".slick-img-").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    
-  });
+//   $(".slick-img-").slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//     infinite: false,
+//   });
   
-  $(".img-slick").slick({
-    slidesToShow: 13.7,
-    slidesToScroll: 4,
-    autoplay: false,
-    autoplaySpeed: 2000,
-    arrows: true,
-    infinite: false,
-    prevArrow: $(".img-slick .prev"),
-    nextArrow: $(".img-slick .next"),
-  });
+//   $(".img-slick").slick({
+//     slidesToShow: 13.7,
+//     slidesToScroll: 4,
+//     autoplay: false,
+//     autoplaySpeed: 2000,
+//     arrows: true,
+//     infinite: false,
+//     prevArrow: $(".img-slick .prev"),
+//     nextArrow: $(".img-slick .next"),
+//   });
   
-  $(".item-container").slick({
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    autoplay: false,
-    autoplaySpeed: 2000,
-    arrows: true,
-    prevArrow: $(".item-container .prev"),
-    nextArrow: $(".item-container .next"),
-  });
+//   $(".item-container").slick({
+//     slidesToShow: 4,
+//     slidesToScroll: 4,
+//     autoplay: false,
+//     autoplaySpeed: 2000,
+//     arrows: true,
+//     prevArrow: $(".item-container .prev"),
+//     nextArrow: $(".item-container .next"),
+//   });
   
-  $(".interior-img").slick({
-    slidesToShow: 6,
-    slidesToScroll: 6,
-    autoplay: false,
-    autoplaySpeed: 2000,
-    arrows: true,
-    infinite: false,
-    arrows: true,
-    prevArrow: $(".interior-img .prev"),
-    nextArrow: $(".interior-img .next"),
-  });
+  
 
-  $(".exh-area").slick({
-    slidesToShow: 4,
-    slidesToScroll: 2,
-    autoplay: false,
-    autoplaySpeed: 2000,
-    arrows: true,
-    infinite: false,
-    arrows: true,
-    prevArrow: $(".exh-area .prev"),
-    nextArrow: $(".exh-area .next"),
-  });
+//   $(".exh-area").slick({
+//     slidesToShow: 4,
+//     slidesToScroll: 2,
+//     autoplay: false,
+//     autoplaySpeed: 2000,
+//     arrows: true,
+//     infinite: false,
+//     arrows: true,
+//     prevArrow: $(".exh-area .prev"),
+//     nextArrow: $(".exh-area .next"),
+//   });
  
-    const slickSlider = $(".cate-list");
-    const slidesToShow = 15;
+//     const slickSlider = $(".cate-list");
+//     const slidesToShow = 15;
 
-    slickSlider.slick({
-      infinite: false,
-      slidesToShow: slidesToShow,
-      slidesToScroll: slidesToShow,
-      adaptiveHeight: false,
-      arrows: false,
-      draggable: false
-    });
+//     slickSlider.slick({
+//       infinite: false,
+//       slidesToShow: slidesToShow,
+//       slidesToScroll: slidesToShow,
+//       adaptiveHeight: false,
+//       arrows: false,
+//       draggable: false
+//     });
 
-    let slideCount = slickSlider.slick("getSlick").slideCount;
-    let currentSlide = 0;
+//     let slideCount = slickSlider.slick("getSlick").slideCount;
+//     let currentSlide = 0;
 
-    console.log(slideCount);
+//     console.log(slideCount);
 
-    $(".prev").hide();
-    $(".prev").click(function () {
-      if (currentSlide > 0) {
-        currentSlide -= slidesToShow;
-        $(".cate-list").slick("slickGoTo", currentSlide);
+//     $(".prev").hide();
+//     $(".prev").click(function () {
+//       if (currentSlide > 0) {
+//         currentSlide -= slidesToShow;
+//         $(".cate-list").slick("slickGoTo", currentSlide);
 
-        if (currentSlide === 0) {
-          $(".prev").hide();
-        }
+//         if (currentSlide === 0) {
+//           $(".prev").hide();
+//         }
 
-        $(".next").show();
-      }
+//         $(".next").show();
+//       }
 
       
-    });
+//     });
 
-    $(".next").click(function () {
-      if (currentSlide < slideCount - slidesToShow) {
-        currentSlide += slidesToShow;
-        $(".cate-list").slick("slickGoTo", currentSlide);
+//     $(".next").click(function () {
+//       if (currentSlide < slideCount - slidesToShow) {
+//         currentSlide += slidesToShow;
+//         $(".cate-list").slick("slickGoTo", currentSlide);
 
-        if (currentSlide >= slideCount - slidesToShow) {
-          $(".next").hide();
-        }
+//         if (currentSlide >= slideCount - slidesToShow) {
+//           $(".next").hide();
+//         }
 
-        $(".prev").show();
-      }
-    });
+//         $(".prev").show();
+//       }
+//     });
 
-  const endTime = new Date();
-        endTime.setTime(endTime.getTime() + 8 * 60 * 60 * 1000);
-        console.log(endTime);
+//   const endTime = new Date();
+//         endTime.setTime(endTime.getTime() + 8 * 60 * 60 * 1000);
+//         console.log(endTime);
 
-        setInterval(() => {
-          const currentTime = new Date().getTime();
-          const remainingTime = endTime - currentTime;
+//         setInterval(() => {
+//           const currentTime = new Date().getTime();
+//           const remainingTime = endTime - currentTime;
 
-          let hours = Math.floor(
-            (remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-          );
-          let minutes = Math.floor(
-            (remainingTime % (1000 * 60 * 60)) / (1000 * 60)
-          );
-          let seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+//           let hours = Math.floor(
+//             (remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+//           );
+//           let minutes = Math.floor(
+//             (remainingTime % (1000 * 60 * 60)) / (1000 * 60)
+//           );
+//           let seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
 
-          hours = hours < 10 ? "0" + hours : hours;
-          minutes = minutes < 10 ? "0" + minutes : minutes;
-          seconds = seconds < 10 ? "0" + seconds : seconds;
+//           hours = hours < 10 ? "0" + hours : hours;
+//           minutes = minutes < 10 ? "0" + minutes : minutes;
+//           seconds = seconds < 10 ? "0" + seconds : seconds;
 
-          document.getElementsByClassName("hours").textContent = hours;
-          document.getElementsByClassName("minutes").textContent = minutes;
-          document.getElementsByClassName("seconds").textContent = seconds;
-        }, 1000);
+//           document.getElementsByClassName("hours").textContent = hours;
+//           document.getElementsByClassName("minutes").textContent = minutes;
+//           document.getElementsByClassName("seconds").textContent = seconds;
+//         }, 1000);
 
         
-});
+// });
