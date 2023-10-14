06dd553a0965b6c3c471d8d8aa1b16f6fe42b0ce
noweaj31 <noweajeel31@gmail.com>
2023-10-14 15:28:40 +0900

    feat:time추가

--- js/time.js
+++ js/time.js
@@ -2,7 +2,19 @@
 
 const endTime = new Date();
 endTime.setTime(endTime.getTime() + 8 * 60 * 60 * 1000);
-console.log(endTime);
+const $hours = document.querySelectorAll(".hours");
+const $minutes = document.querySelectorAll(".minutes");
+const $seconds = document.querySelectorAll(".seconds");
+
+function formatTime(element) {
+  return element < 10 ? "0" + element : element;
+}
+
+function updateDisplay(elements, value) {
+  elements.forEach((element) => {
+    element.textContent = formatTime(value);
+  });
+}
 
 setInterval(() => {
   const currentTime = new Date().getTime();
@@ -14,11 +26,7 @@ setInterval(() => {
   let minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
 
-  hours = hours < 10 ? "0" + hours : hours;
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  seconds = seconds < 10 ? "0" + seconds : seconds;
-
-  document.getElementsByClassName("hours").textContent = hours;
-  document.getElementsByClassName("minutes").textContent = minutes;
-  document.getElementsByClassName("seconds").textContent = seconds;
+  updateDisplay($hours, hours);
+  updateDisplay($minutes, minutes);
+  updateDisplay($seconds, seconds);
 }, 1000);
