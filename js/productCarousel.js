b8852e41db6e3ccaeb60427ec152ead54d9aa8ea
noweaj31 <noweajeel31@gmail.com>
2023-11-30 20:31:05 +0900

    feat:gallery carousel

--- js/productCarousel.js
+++ js/productCarousel.js
@@ -12,3 +12,24 @@ const productCarousel = tns({
   mouseDrag: true,
   preventScrollOnTouch: true,
 });
+
+// usergallery
+
+const userGallery = tns({
+  container: ".user-gallery-slider .slider-list",
+  gutter: 4,
+  edgePadding: 16,
+  loop: false,
+  controls: false,
+  arrowKeys: true,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+  items: 1,
+  responsive: {
+    768: {
+      gutter: 6,
+      edgePadding: 75,
+      controls: true,
+    },
+  },
+});
