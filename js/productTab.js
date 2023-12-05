7b96951c693488bccad6d4034abf72f34efe6b13
noweaj31 <noweajeel31@gmail.com>
2023-12-06 02:22:59 +0900

    feat:panel position

--- js/productTab.js
+++ js/productTab.js
@@ -1,12 +1,12 @@
 "use strict";
 
 const productTab = document.querySelector(".product-selling-nav");
-const productTabBtns = productTab.querySelectorAll("button");
-
-let currentActiveTab = productTab.querySelector(".is_active");
+const productTabButtonList = productTab.querySelectorAll("button");
 
 const TOP_HEADER_DESKTOP = 80 + 48 + 52;
 const TOP_HEADER_MOBILE = 50 + 40 + 52;
+
+let currentActiveTab = productTab.querySelector(".is_active");
 
 function toggleActiveTab() {
   const tabItem = this.parentNode;
@@ -32,7 +32,7 @@ function scrollToTabPanel() {
   });
 }
 
-productTabBtns.forEach((button) => {
+productTabButtonList.forEach((button) => {
   button.addEventListener("click", toggleActiveTab);
   button.addEventListener("click", scrollToTabPanel);
 });
@@ -58,10 +58,40 @@ const productTabPanelPositionMap = {};
 function detectTabPanelPosition() {
   productTabPanelList.forEach((panel) => {
     const id = panel.getAttribute("id");
-    const position = window.scrollY + element.getBoundingClientRect().top;
+    const position = window.scrollY + panel.getBoundingClientRect().top;
     productTabPanelPositionMap[id] = position;
   });
 }
 
+function updateActiveTabOnScroll() {
+  const scrolledAmount =
+    window.scrollY +
+    (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP : TOP_HEADER_MOBILE);
+
+  let newActiveTab;
+
+  if (scrolledAmount >= productTabPanelPositionMap["product-recommendation"]) {
+    newActiveTab = productTabButtonList[4];
+  } else if (scrolledAmount >= productTabPanelPositionMap["product-shipment"]) {
+    newActiveTab = productTabButtonList[3];
+  } else if (scrolledAmount >= productTabPanelPositionMap["product-inquiry"]) {
+    newActiveTab = productTabButtonList[2];
+  } else if (scrolledAmount >= productTabPanelPositionMap["product-review"]) {
+    newActiveTab = productTabButtonList[1];
+  } else {
+    newActiveTab = productTabButtonList[0];
+  }
+
+  if (newActiveTab) {
+    newActiveTab = newActiveTab.parentNode;
+
+    if (newActiveTab !== currentActiveTab) {
+      newActiveTab.classList.add("is_active");
+      currentActiveTab.classList.remove("is_active");
+      currentActiveTab = newActiveTab;
+    }
+  }
+}
 window.addEventListener("load", detectTabPanelPosition);
 window.addEventListener("resize", detectTabPanelPosition);
+window.addEventListener("scroll", updateActiveTabOnScroll);
