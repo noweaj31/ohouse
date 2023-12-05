b1cd69d530de21c4f973e3c1334ba444a8798125
noweaj31 <noweajeel31@gmail.com>
2023-12-05 23:28:54 +0900

    feat: tabpanel position

--- js/productTab.js
+++ js/productTab.js
@@ -36,3 +36,32 @@ productTabBtns.forEach((button) => {
   button.addEventListener("click", toggleActiveTab);
   button.addEventListener("click", scrollToTabPanel);
 });
+
+// 각 tabpanel y축의 위치 (문서의 시작점에서부터 얼마나 아래에 있는지)
+// 요소의 y축 위치 = window.scrollY + element.getBoundingClientRect().top
+
+const productTabPanelIdList = [
+  "product-spec",
+  "product-review",
+  "product-inquiry",
+  "product-shipment",
+  "product-recommendation",
+];
+
+const productTabPanelList = productTabPanelIdList.map((panelId) => {
+  const tabPanel = document.querySelector(`#${panelId}`);
+  return tabPanel;
+});
+
+const productTabPanelPositionMap = {};
+
+function detectTabPanelPosition() {
+  productTabPanelList.forEach((panel) => {
+    const id = panel.getAttribute("id");
+    const position = window.scrollY + element.getBoundingClientRect().top;
+    productTabPanelPositionMap[id] = position;
+  });
+}
+
+window.addEventListener("load", detectTabPanelPosition);
+window.addEventListener("resize", detectTabPanelPosition);
